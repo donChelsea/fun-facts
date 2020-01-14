d2d2c7068e807e8c336f2302e57960d592a302dc
Tafadzwa Katsidzira <tafadzwakatsidzira@Tafadzwas-MBP-2.home>
2020-01-14 02:31:24 -0500

    initial commit

---
+++ app/src/main/java/com/katsidzira/funfacts/FunFactsActivity.kt
@@ -0,0 +1,14 @@
+package com.katsidzira.funfacts
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fun_facts)
+    }
+
+
+}
